d12a732a9517aae7af673aa0ed936e77c5fd4ccb
Rossy Nallely Bautista Vazquez <rossybautista167@gmail.com>
2026-04-26 21:24:50 -0600

    Examen Parcial Unidad 2: PDO

---
+++ parcial-2-pdo/examen-practico/Examen-Parcial-Unidad-2-PDO/config/Database.php
@@ -0,0 +1,30 @@
+<?php
+namespace config;
+//Bautista Vazquez Rossy Nallely
+use PDO;
+use PDOException;
+
+class Database {
+    private $host = "localhost";
+    private $dbname = "productos2";
+    private $username = "root";
+    private $password = "";
+    private $connection;
+
+    public function __construct() {
+        try {
+            $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset=utf8mb4";
+            $this->connection = new PDO($dsn, $this->username, $this->password);
+
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        } catch (PDOException $e) {
+            die("Error de conexión: " . $e->getMessage());
+        }
+    }
+
+    public function getConnection() {
+        return $this->connection;
+    }
+}
---
+++ parcial-2-pdo/examen-practico/Examen-Parcial-Unidad-2-PDO/vendor/autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+spl_autoload_register(function ($class) {
+    $ruta = str_replace("\\", "/", $class) . ".php";
+
+    if (file_exists($ruta)) {
+        require_once $ruta;
+    }
+});
